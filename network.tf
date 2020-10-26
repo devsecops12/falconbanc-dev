1f91062cbc4f40e7789666deb66ad5cf8497c38e
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 16:26:36 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-resource "ibm_is_vpc" "iac_iks_vpc" {
+resource "ibm_is_vpc" "iac_iks_vpc_1" {
   name = "${var.project_name}-${var.environment}-vpc"
   resource_group = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
@@ -8,7 +8,7 @@ resource "ibm_is_vpc_address_prefix" "vpc_address_prefix" {
   count                     = local.max_size
   name                      = "${var.project_name}-${var.environment}-range-${format("%02s", count.index)}"
   zone                      = var.vpc_zone_names[count.index]
-  vpc                       = ibm_is_vpc.iac_iks_vpc.id
+  vpc                       = ibm_is_vpc.iac_iks_vpc_1.id
   cidr                      = "172.26.${format("%01s", count.index)}.0/24"
 }
 
@@ -16,7 +16,7 @@ resource "ibm_is_subnet" "iac_iks_subnet" {
   count                    = local.max_size
   name                     = "${var.project_name}-${var.environment}-subnet-${format("%02s", count.index)}"
   zone                     = var.vpc_zone_names[count.index]
-  vpc                      = ibm_is_vpc.iac_iks_vpc.id
+  vpc                      = ibm_is_vpc.iac_iks_vpc_1.id
   ipv4_cidr_block          = "172.26.${format("%01s", count.index)}.0/26"
   # total_ipv4_address_count = 64
   resource_group           = data.ibm_resource_group.group.id
@@ -25,7 +25,7 @@ resource "ibm_is_subnet" "iac_iks_subnet" {
   depends_on  = [ibm_is_vpc_address_prefix.vpc_address_prefix]
 }
 
-resource "ibm_is_vpc" "iac_iks_vpc-2" {
+resource "ibm_is_vpc" "iac_iks_vpc_2" {
   name = "${var.project_name}-${var.environment}-vpc"
   resource_group = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
@@ -33,7 +33,7 @@ resource "ibm_is_vpc" "iac_iks_vpc-2" {
 
 resource "ibm_is_security_group_rule" "iac_iks_security_group_rule_tcp_k8s" {
   count     = local.max_size
-  group     = ibm_is_vpc.iac_iks_vpc-1.default_security_group
+  group     = ibm_is_vpc.iac_iks_vpc_1.default_security_group
   direction = "inbound"
   remote    = ibm_is_subnet.iac_iks_subnet[count.index].ipv4_cidr_block
 
@@ -52,7 +52,7 @@ resource "ibm_tg_gateway" "new_tg_gw"{
 
 resource "ibm_is_public_gateway" "iac_iks_gateway" {
     name  = "${var.project_name}-${var.environment}-gateway-${format("%02s", count.index)}"
-    vpc   = ibm_is_vpc.iac_iks_vpc-1.id
+    vpc   = ibm_is_vpc.iac_iks_vpc_1.id
     zone  = var.vpc_zone_names[count.index]
     count = local.max_size
 
